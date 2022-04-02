77ad7181a944e7759095f365a9aa1226a0cc596e
Chae Haram <djvemfla2@gmail.com>
2022-04-02 11:45:17 +0900

    달력에 점찍기

--- MyCalendar/MyCalendar/ViewController.swift
+++ MyCalendar/MyCalendar/ViewController.swift
@@ -18,7 +18,7 @@ import FSCalendar
 
 class ViewController: UIViewController {
     
-    var dateFormatter: DateFormatter {
+    var dateFormatter: DateFormatter = {
         let df = DateFormatter()
         df.dateFormat = "yyyy/MM/dd"
         df.locale = Locale(identifier: "ko-KR")
@@ -56,16 +56,16 @@ class ViewController: UIViewController {
     
     func makeTestData() {
         let list: [Schedule] = [
-              Schedule(time: "오후 12시", title: "점심 먹기", date: strToDate(str: "2022-04-01")),
-              Schedule(time: "오후  1시", title: "발표 하기", date: strToDate(str: "2022-04-01")),
-              Schedule(time: "오후  3시", title: "프로젝트 회의", date: strToDate(str: "2022-04-01")),
-              Schedule(time: "오전  8시", title: "기상", date: strToDate(str: "2022-04-04")),
-              Schedule(time: "오전 10시", title: "모의 시험", date: strToDate(str: "2022-04-04")),
-              Schedule(time: "오후  1시", title: "스터디", date: strToDate(str: "2022-04-06")),
-              Schedule(time: "오후  3시", title: "과외", date: strToDate(str: "2022-04-11")),
-              Schedule(time: "오후  5시", title: "과제하기", date: strToDate(str: "2022-04-13")),
-              Schedule(time: "오전 11시", title: "본가 가기", date: strToDate(str: "2022-04-13")),
-              Schedule(time: "오후  7시", title: "00이랑 술 약속", date: strToDate(str: "2022-04-20"))
+              Schedule(time: "오후 12시", title: "점심 먹기", date: strToDate(str: "2022/04/01")),
+              Schedule(time: "오후  1시", title: "발표 하기", date: strToDate(str: "2022/04/01")),
+              Schedule(time: "오후  3시", title: "프로젝트 회의", date: strToDate(str: "2022/04/01")),
+              Schedule(time: "오전  8시", title: "기상", date: strToDate(str: "2022/04/04")),
+              Schedule(time: "오전 10시", title: "모의 시험", date: strToDate(str: "2022/04/04")),
+              Schedule(time: "오후  1시", title: "스터디", date: strToDate(str: "2022/04/06")),
+              Schedule(time: "오후  3시", title: "과외", date: strToDate(str: "2022/04/11")),
+              Schedule(time: "오후  5시", title: "과제하기", date: strToDate(str: "2022/04/13")),
+              Schedule(time: "오전 11시", title: "본가 가기", date: strToDate(str: "2022/04/13")),
+              Schedule(time: "오후  7시", title: "00이랑 술 약속", date: strToDate(str: "2022/04/20"))
             ]
         list.forEach { schedule in
             MyDB.dataList.append(schedule)
@@ -79,10 +79,26 @@ class ViewController: UIViewController {
 
 extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
     func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        
+        for schedule in MyDB.dataList {
+            let eventDate = schedule.date
+            if eventDate == date {
+                let count = MyDB.dataList.filter { schedule in
+                    schedule.date == date
+                }.count
+                
+                if count >= 3 {
+                    return 3
+                } else {
+                    return count
+                }
+            }
+        }
+        
         return 0
     }
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        
+        dateLabel.text = dateFormatter.string(from: date)
             // 화면 전환 코드, 데이터 리로드 코드
         
     }
