30cd06d2ffa7cfe003938c4abfa457f295fb2584
Chae Haram <djvemfla2@gmail.com>
2022-05-08 21:42:43 +0900

    chart 기본 세팅

---
+++ ChartExample/ChartExample/BarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BarViewController.swift
+//  ChartExample
+//
+//  Created by Chae_Haram on 2022/05/08.
+//
+
+import UIKit
+
+class BarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
---
+++ ChartExample/ChartExample/LineViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LineViewController.swift
+//  ChartExample
+//
+//  Created by Chae_Haram on 2022/05/08.
+//
+
+import UIKit
+
+class LineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
