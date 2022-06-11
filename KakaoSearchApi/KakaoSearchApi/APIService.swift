fccd061737cf2a172470f33173d38a8fa71012f3
Chae Haram <djvemfla2@gmail.com>
2022-06-11 22:05:37 +0900

    모델 생성 및 APIService 파일 생성

---
+++ KakaoSearchApi/KakaoSearchApi/APIService.swift
@@ -0,0 +1,19 @@
+//
+//  ApiService.swift
+//  KakaoSearchApi
+//
+//  Created by Chae_Haram on 2022/06/11.
+//
+
+import Foundation
+
+struct APIService {
+    let baseUrl: String = "https://dapi.kakao.com/v3/search/book?target=title"
+    
+    
+    func searchBook(query: String) {
+        
+    }
+}
+
+
---
+++ KakaoSearchApi/KakaoSearchApi/Book.swift
@@ -0,0 +1,13 @@
+//
+//  Book.swift
+//  KakaoSearchApi
+//
+//  Created by Chae_Haram on 2022/06/11.
+//
+
+import Foundation
+
+struct Book: Decodable {
+    let title: String
+    let contents: String
+}
