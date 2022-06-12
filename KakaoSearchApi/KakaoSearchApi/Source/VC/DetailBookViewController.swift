2244c1345104aa92b1e2f97815a52fa9e8c33b04
Chae Haram <djvemfla2@gmail.com>
2022-06-13 03:19:00 +0900

    [ADD] DetailViewController : 넘겨온 값 화면에 뿌려주기

--- KakaoSearchApi/KakaoSearchApi/Source/VC/DetailBookViewController.swift
+++ KakaoSearchApi/KakaoSearchApi/Source/VC/DetailBookViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class DetailBookViewController: UIViewController {
 
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var bookPublisher: UILabel!
+    @IBOutlet weak var bookAuthor: UILabel!
+    @IBOutlet weak var bookContent: UILabel!
+    
+    var detailBook: Book?
+    var author: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        guard let detailBook = detailBook else { return }
 
+        for data in detailBook.authors {
+            author += data
+        }
+        DetailbookSet()
+    }
+    func DetailbookSet() {
+        if let detailBook = detailBook {
+            bookImage.image = UIImage(named: detailBook.thumbnail)
+            bookTitle.text = detailBook.title
+            bookPublisher.text = detailBook.publisher
+            bookAuthor.text = author
+            bookContent.text = detailBook.contents
+        }
     }
 
 }
